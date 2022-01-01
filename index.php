c01da8defed9e153fa38c6318206dcc2a50fa161
BEN-ISATU <96709820+BEN-ISATU@users.noreply.github.com>
2022-01-01 21:42:36 +0800

    Add files via upload
    BEN P. NICOLAS-CRUD PROJECT

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+
+// Home Page template below.//
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p><strong>Welcome to InfoSaved!</strong></p>
+    <p>Where you can create, read, update and delete your information.</p>
+</div>
+
+<?=template_footer()?>
